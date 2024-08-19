2c34ec700277df658bf6af05eea739032dbcb079
ninad pradhan <ninadpradhan@auditocity.com>
2024-08-18 23:01:50 -0700

    fixing amplify version

--- saas-template/front-end/deployment/config/config-development.ts
+++ saas-template/front-end/deployment/config/config-development.ts
@@ -1,7 +1,7 @@
 /**
  * Add your hosted dev URL here
  */
-const HOSTED_URL = 'https://dev.domain.com';
+const HOSTED_URL = 'https://main.db2w9kmfi5hov.amplifyapp.com';
 
 const configDevelopment = {
   HOSTED_URL,
@@ -14,10 +14,12 @@ const configDevelopment = {
   /**
    * Add the details from the Pulumi output here, after running 'pulumi up'
    */
-  USER_POOL_CLIENT_ID: '',
-  USER_POOL_ID: '',
-  IDENTITY_POOL_ID: '',
-  GRAPHQL_ENDPOINT: '',
+  USER_POOL_CLIENT_ID: '5vvpees0j8uab34mgec3lnnbph',
+  USER_POOL_ID: 'us-west-1_1pDqTrKJn',
+  IDENTITY_POOL_ID: 'us-west-1:6e256c29-8072-4dab-ae74-81601b004160',
+  GRAPHQL_ENDPOINT: {
+    GRAPHQL : 'https://grl4u7o5jzb5bpenatsi43qjge.appsync-api.us-west-1.amazonaws.com/graphql',
+    REALTIME: 'wss://grl4u7o5jzb5bpenatsi43qjge.appsync-realtime-api.us-west-1.amazonaws.com/graphql' }
 };
 
 export default configDevelopment;
