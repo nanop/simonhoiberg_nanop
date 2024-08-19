a6114526f064439683ce4bd3ab5c5655f1b88e3a
ninad pradhan <ninadpradhan@auditocity.com>
2024-08-18 21:02:16 -0700

    first push

---
+++ saas-template/front-end/graphql/API.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+//  This file was automatically generated and should not be edited.
+
+export type CognitoUser = {
+  __typename: "CognitoUser",
+  id: string,
+  username: string,
+};
+
+export type GetCognitoUserQueryVariables = {
+};
+
+export type GetCognitoUserQuery = {
+  getCognitoUser?:  {
+    __typename: "CognitoUser",
+    id: string,
+    username: string,
+  } | null,
+};
